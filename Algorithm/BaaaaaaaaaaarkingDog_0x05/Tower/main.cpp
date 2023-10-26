d8f0e612769aa84545a8512ea7c73d38b0b844ae
BangKyungUn <138852021+SPEAROW486@users.noreply.github.com>
2023-10-26 21:54:38 +0900

    daily coding 2023/10/26

--- Algorithm/BaaaaaaaaaaarkingDog_0x05/Tower/main.cpp
+++ Algorithm/BaaaaaaaaaaarkingDog_0x05/Tower/main.cpp
@@ -3,10 +3,14 @@
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    
     int n = 0;
     std::cin >> n;
 
-    std::stack<int> towers;
+    std::stack<std::pair<int, int>> towers;
     for (int i = 0; i < n; ++i)
     {
         int top = 0;
@@ -14,12 +18,19 @@ int main()
 
         while (!towers.empty())
         {
+            if (towers.top().first > top)
+            {
+                std::cout << towers.top().second << " ";
+                break;
+            }
+            towers.pop();
         }
 
         if (towers.empty())
         {
             std::cout << 0 << " ";
         }
+        towers.push(std::pair<int, int>(top, i + 1));
     }
 
     return 0;
