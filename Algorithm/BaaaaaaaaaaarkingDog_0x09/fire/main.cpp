b57626382b1cb34807f07846e5c18cc984de5717
BangKyungUn <138852021+SPEAROW486@users.noreply.github.com>
2024-01-31 21:47:22 +0900

    baekjoon 4179

--- Algorithm/BaaaaaaaaaaarkingDog_0x09/fire/main.cpp
+++ Algorithm/BaaaaaaaaaaarkingDog_0x09/fire/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
+#include <limits.h>
 
 struct Point
 {
@@ -39,75 +41,78 @@ int main()
     int r, c;
     std::cin >> r >> c;
 
-    char maze[4][4] = {0};
-    int buf_j[4][4] = {0};
-    int buf_f[4][4] = {0};
+    const int BUF_SIZE = 1000;
+    char maze[BUF_SIZE][BUF_SIZE];
+    bool buf_j[BUF_SIZE][BUF_SIZE];
+    int buf_f[BUF_SIZE][BUF_SIZE];
+
+    std::fill(&buf_j[0][0], &buf_j[0][0] + sizeof(buf_j) / sizeof(bool), 0);
+    std::fill(&buf_f[0][0], &buf_f[0][0] + sizeof(buf_f) / sizeof(int), INT_MAX);
 
     std::queue<Point> queue_f;
-    std::queue<Point> queue_j;
+    std::queue<std::pair<Point, int>> queue_j;
     for (int i = 0; i < r; ++i)
     {
         for (int j = 0; j < c; ++j)
         {
-            bool is_fire = false;
             std::cin >> maze[i][j];
 
             if (maze[i][j] == 'J')
             {
-                queue_j.push(Point(i, j));
+                queue_j.push(std::pair<Point, int>(Point(i, j), 0));
             }
 
             if (maze[i][j] == 'F')
             {
                 queue_f.push(Point(i, j));
-                is_fire = true;
                 buf_f[i][j] = 0;
-            }
-
-            if (!is_fire)
-            {
-                buf_f[i][j] = INT_MAX;
             }
         }
     }
 
-    // 1. bfs buf_f
-    // 2. bfs buf_j
-
-    while (!queue_j.empty())
+    while (!queue_f.empty())
     {
-        Point point_f = queue_f.front();
-        Point point_j = queue_j.front();
+        Point p = queue_f.front();
         for (const Point &direction : SearchDirection)
         {
-            Point UnitTest_f = point_f + direction;
-            if (IsContain(UnitTest_f, r, c))
+            Point u = p + direction;
+            if (IsContain(u, r, c))
             {
                 // 지나갈수 있는 공간
-                if (maze[UnitTest_f.row][UnitTest_f.col] == '.' && buf_f[UnitTest_f.row][UnitTest_f.col] == 0)
+                if (maze[u.row][u.col] == '.' && buf_f[u.row][u.col] == INT_MAX)
                 {
-                    queue_f.push(UnitTest_f);
-                    buf_f[UnitTest_f.row][UnitTest_f.col] = buf_f[point_f.row][point_f.col] + 1;
-                }
-            }
-
-            Point UnitTest_j = point_j + direction;
-            if (IsContain(UnitTest_j, r, c))
-            {
-                if (maze[UnitTest_j.row][UnitTest_j.col] == '.' && buf_j[UnitTest_j.row][UnitTest_j.col] == 0 && buf_f[UnitTest_j.row][UnitTest_j.col] == 0)
-                {
-                    queue_j.push(UnitTest_j);
-                    buf_j[UnitTest_j.row][UnitTest_j.col] = buf_j[point_j.row][point_j.col] + 1;
-
-                    if (UnitTest_j.row == 0 || UnitTest_j.row == r - 1 || UnitTest_j.col == 0 || UnitTest_j.col == c - 1)
-                    {
-                        std::cout << buf_j[UnitTest_j.row][UnitTest_j.col];
-                        return 0;
-                    }
+                    queue_f.push(u);
+                    buf_f[u.row][u.col] = buf_f[p.row][p.col] + 1;
                 }
             }
         }
         queue_f.pop();
+    }
+
+    while (!queue_j.empty())
+    {
+        Point p = queue_j.front().first;
+        int minimum_time = queue_j.front().second;
+
+        if (p.row == 0 || p.row == r - 1 || p.col == 0 || p.col == c - 1)
+        {
+            std::cout << minimum_time + 1;
+            return 0;
+        }
+
+        for (const Point &direction : SearchDirection)
+        {
+            Point u = p + direction;
+            if (IsContain(u, r, c))
+            {
+                if (maze[u.row][u.col] == '.' && buf_f[u.row][u.col] > minimum_time + 1 && buf_j[u.row][u.col] == false)
+                {
+                    queue_j.push(std::pair<Point, int>(u, minimum_time + 1));
+                    buf_j[u.row][u.col] = true;
+                }
+            }
+        }
+
         queue_j.pop();
     }
 
