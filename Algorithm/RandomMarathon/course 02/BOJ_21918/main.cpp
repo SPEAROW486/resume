930e8a5c7c0eb28240b0a1fb816767cbfb416b57
SPEAROW486 <138852021+SPEAROW486@users.noreply.github.com>
2024-06-16 00:07:18 +0900

    rm 02

--- Algorithm/RandomMarathon/course 02/BOJ_21918/main.cpp
+++ Algorithm/RandomMarathon/course 02/BOJ_21918/main.cpp
@@ -1,22 +1,60 @@
 
 #include <bits/stdc++.h>
 
-int r, c, n;
+int n, m;
+constexpr int MAX = 4001;
+int buf[MAX];
 
 int main()
 {
-    // std::ios_base::sync_with_stdio(false);
-    // std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-    std::cin >> r >> c >> n;
+    std::cin >> n >> m;
 
-    long long rn = r % n != 0 ? r / n + 1 : r / n;
-    long long cn = c % n != 0 ? c / n + 1 : c / n;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> buf[i];
+    }
 
-    std::cout << rn * cn;
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        if (a == 1)
+        {
+            buf[b - 1] = c;
+        }
+        else if (a == 2)
+        {
+            for (int j = b - 1; j < c; ++j)
+            {
+                buf[j] = !buf[j];
+            }
+        }
+        else if (a == 3)
+        {
+            for (int j = b - 1; j < c; ++j)
+            {
+                buf[j] = 0;
+            }
+        }
+        else
+        {
+            for (int j = b - 1; j < c; ++j)
+            {
+                buf[j] = 1;
+            }
+        }
+    }
 
-    int nn;
-    std::cin >> nn;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << buf[i] << " ";
+    }
+
+    // int nn;
+    // std::cin >> nn;
 
     return 0;
 }
