f46027fbb14c0e7b4d98e66e57763a26f0ad9d62
SPEAROW486 <138852021+SPEAROW486@users.noreply.github.com>
2024-08-16 19:54:22 +0900

    boj

--- Algorithm/BOJ/BOJ_1205/main.cpp
+++ Algorithm/BOJ/BOJ_1205/main.cpp
@@ -5,54 +5,44 @@
     std::ios_base::sync_with_stdio(false); \
     std::cin.tie(NULL)
 
-int n, s, p;
-std::vector<int> v;
+int n;
+std::vector<std::string> v;
 
 int main()
 {
-    //    FASTIO;
-
-    std::cin >> n >> s >> p;
-
+    FASTIO;
+    std::cin >> n;
+    int length = 0;
     for (int i = 0; i < n; ++i)
     {
-        int tmp;
+        std::string tmp;
         std::cin >> tmp;
         v.push_back(tmp);
+
+        length = tmp.length();
     }
 
-    // 새로운 점수가 몇 등인지 계산
-    int rank = 1;  // 1등부터 시작
-    int count = 0; // 리스트에 들어갈 수 있는 점수 개수 카운트
-
-    for (int i = 0; i < n; ++i)
+    // 최소 1자리는 남겨야 할테니까?
+    // 루프안에서 정답을 못찾으면 입력 길이 그대로임.
+    for (int i = 1; i < length; ++i)
     {
-        if (v[i] > s)
+        std::unordered_set<std::string> set;
+        for (int j = 0; j < v.size(); ++j)
         {
-            rank++;
+            std::string tmp = v[j].substr(v[j].length() - i, i);
+            set.insert(tmp);
         }
-        else if (v[i] == s)
+
+        if (set.size() == n)
         {
-            rank = rank; // 같은 점수일 경우 등수는 변하지 않음
+            std::cout << i << "\n";
+            return 0;
         }
-        else
-        {
-            break; // 새로운 점수가 더 크다면 그 이후의 점수는 비교하지 않음
-        }
-        count++;
     }
 
-    // P가 꽉 차고, 새로운 점수가 들어갈 자리가 없는 경우
-    if (count == p && v[n - 1] >= s)
-    {
-        std::cout << -1 << std::endl;
-    }
-    else
-    {
-        std::cout << rank << std::endl;
-    }
+    std::cout << length << "\n";
 
-    int nn;
-    std::cin >> nn;
+    // int nn;
+    // std::cin >> nn;
     return 0;
 }
