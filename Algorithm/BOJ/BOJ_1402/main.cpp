86c470f9e4bf02af6f9583e2ce6adbb243a8f00a
SPEAROW486 <138852021+SPEAROW486@users.noreply.github.com>
2024-10-15 18:05:40 +0900

    boj

---
+++ Algorithm/BOJ/BOJ_1402/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define FASTIO                             \
+    std::ios_base::sync_with_stdio(false); \
+    std::cin.tie(NULL)
+
+// BOJ : 1402
+int main() {
+    FASTIO;
+
+    int T;
+    std::cin >> T;
+    
+    for (int i = 0; i < T; ++i) {
+        int A, B;
+        std::cin >> A >> B;
+        std::cout << "yes" << '\n';
+    }
+
+    return 0;
+}
